2c4e2719fb58bcac424da77eb09c36de536ab517
Fahim Bin-Afzal Al-Ashrafi <fahimrafi37@gmail.com>
2021-08-04 21:22:26 +0600

    Update 06 forms

---
+++ 06 forms/base.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, url_for, render_template, flash, session, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'openkey'
+
+class SimpleForm(FlaskForm):
+    breed = StringField("What breed of pups are you looking for? ")
+    submit = SubmitField("Submit")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    breed = False
+    form = InfoForm()
+    if form.validate_on_submit():
+        breed = form.breed.data
+        form.breed.data = ""
+    return render_template('index.html', form=form, breed=breed)
+
+if __name__ == '__main__':
+    app.run(debug=True)
