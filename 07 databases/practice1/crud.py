2c1eb5ad2446529a447217bbf760c93941d6b091
Fahim Bin-Afzal Al-Ashrafi <fahimrafi37@gmail.com>
2021-08-06 17:49:12 +0600

    performed migrations and crud operations to practice1

--- 07 databases/practice1/crud.py
+++ 07 databases/practice1/crud.py
@@ -1,28 +1,34 @@
 from practice import db, Puppy
 
-add_puppy = Puppy("Kukur",3)
-db.session.add(add_puppy)
+# add_puppy = Puppy("Kukur",3)
+# db.session.add(add_puppy)
+# db.session.commit()
+
+# read_puppies = Puppy.query.all()
+# print(read_puppies)
+
+# select_puppy = Puppy.query.get(1)
+# print(select_puppy)
+# print(select_puppy.name)
+
+# filter_puppy = Puppy.query.filter_by(age=3)
+# print(filter_puppy)
+# print(filter_puppy.all())
+
+# update_puppy = Puppy.query.get(1)
+# update_puppy.age = 4
+# db.session.add(update_puppy)
+# db.session.commit()
+
+# delete_puppy = Puppy.query.get(2)
+# db.session.delete(delete_puppy)
+# db.session.commit()
+
+# all_puppies = Puppy.query.all()
+# print(all_puppies)
+
+add_puppy_breed = Puppy.query.get(1)
+add_puppy_breed.breed = "labrador"
+db.session.add(add_puppy_breed)
 db.session.commit()
-
-read_puppies = Puppy.query.all()
-print(read_puppies)
-
-select_puppy = Puppy.query.get(1)
-print(select_puppy)
-print(select_puppy.name)
-
-filter_puppy = Puppy.query.filter_by(age=3)
-print(filter_puppy)
-print(filter_puppy.all())
-
-update_puppy = Puppy.query.get(1)
-update_puppy.age = 4
-db.session.add(update_puppy)
-db.session.commit()
-
-delete_puppy = Puppy.query.get(2)
-db.session.delete(delete_puppy)
-db.session.commit()
-
-all_puppies = Puppy.query.all()
-print(all_puppies)
+print(Puppy.query.get(1).breed)
