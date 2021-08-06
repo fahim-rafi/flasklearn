ac1a01fdd1cd34207f30a06151528ae50aabf5e7
Fahim Bin-Afzal Al-Ashrafi <fahimrafi37@gmail.com>
2021-08-07 01:45:43 +0600

    completed pupper adoption site practice

--- 07 databases/practice3/adoption_site.py
+++ 07 databases/practice3/adoption_site.py
@@ -27,7 +27,7 @@ class Puppy(db.Model):
         self.name = name
 
     def __repr__(self):
-        return f"Name of the puppy is {self.name}"
+        return f"Name of the pupper is {self.name}"
 
 # FORMS
 
