31477ac2a52d4ead2681f9564e74cbc3aad95eb4
fahim-rafi <fahimrafi37@gmail.com>
2021-08-24 23:13:32 +0600

    added picture handler

--- 10 blog/blog/users/picture_handler.py
+++ 10 blog/blog/users/picture_handler.py
@@ -5,4 +5,16 @@ from flask import url_for, current_app
 
 def add_profile_picture(pic_upload, username):
     filename = pic_upload.filename
-    ext_type = filename.split(".")
+    ext_type = filename.split(".")[-1]
+    storage_filename = str(username) + "." + ext_type
+
+    filepath = os.path.join(
+        current_app.root_path, "static/profile_pics", storage_filename
+    )
+
+    output_size = (200, 200)
+    pic = Image.open(pic_upload)
+    pic.thumbnail(output_size)
+    pic.save(filepath)
+
+    return storage_filename
