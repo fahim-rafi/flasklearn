9c9a5f9ad8381fd3a6f36d123fa51444e1967418
fahim-rafi <fahimrafi37@gmail.com>
2021-09-02 23:07:40 +0600

    completed project

--- 10 blog/blog/posts/views.py
+++ 10 blog/blog/posts/views.py
@@ -32,7 +32,7 @@ def blog_post(blog_post_id):
 
 
 # Update
-@blog_posts.route("/<int:blog_post_id/update>", methods=["GET", "POST"])
+@blog_posts.route("/<int:blog_post_id>/update", methods=["GET", "POST"])
 @login_required
 def blog_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
@@ -47,7 +47,7 @@ def blog_post(blog_post_id):
         flash("Post Updated!")
         return redirect(url_for("blog_posts.blog_post", blog_post_id=blog_post_id))
 
-    elif request.method is "GET":
+    elif request.method == "GET":
         form.title.data = blog_post.title
         form.text.data = blog_post.text
 
@@ -55,7 +55,7 @@ def blog_post(blog_post_id):
 
 
 # Delete
-@blog_posts.route("/<int:blog_post_id/delete>", methods=["GET", "POST"])
+@blog_posts.route("/<int:blog_post_id>/delete", methods=["GET", "POST"])
 @login_required
 def delete_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
