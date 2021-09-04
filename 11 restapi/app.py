4635b12f97033e79fbc027ab35aef7263ea73e6b
fahim-rafi <fahimrafi37@gmail.com>
2021-09-04 23:16:40 +0600

    added rest api folder

---
+++ 11 restapi/app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+
+api = Api(app)
+
+puppies = []
+
+
+class PuppyNames(Resource):
+    def post(self, name):
+        pup = {"name": name}
+        puppies.append(pup)
+        return pup
+
+    def get(self, name):
+        for pup in puppies:
+            if pup["name"] == name:
+                return pup
+        return {"name": None}, 404
+
+    def delete(self, name):
+        for ind, pup in enumerate(puppies):
+            if pup["name"] == name:
+                deleted_pup = puppies.pop(ind)
+                print(deleted_pup)
+                return {"note": "delete successful"}
+
+
+class AllNames(Resource):
+    def get(self):
+        return {"puppies": puppies}
+
+
+api.add_resource(PuppyNames, "/puppy/<string:name>")
+api.add_resource(AllNames, "/puppies")
+
+if __name__ == "__main__":
+    app.run(debug=True)
